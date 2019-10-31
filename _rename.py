72af22c27eaa9f828d46b1803c054e36c80c28d5
khpierce <pierce_iii@hotmail.com>
2019-10-31 11:59:50 +0700

    [+] fix : rename file to format data[number]

--- _rename.py
+++ _rename.py
@@ -1,7 +1,9 @@
 import os
-path = 'Appliance/wicker'
+path = 'Wooden_utensils/spoon'
 i = 0
 for filename in os.listdir(path):
     os.rename(os.path.join(path,filename), os.path.join(path,'data'+str(i)+'.jpg'))
     # print(os.path.join(path,filename))
     i = i +1
+
+print('success')
